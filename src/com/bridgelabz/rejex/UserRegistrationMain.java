e9338e52d88ceacf73bfb60d585bdd8987bef37e
kirantidke <kirantidke2013@gmail.com>
2021-06-29 17:12:40 +0530

    "[kiran] added file"

---
+++ src/com/bridgelabz/rejex/UserRegistrationMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.rejex;
+ import java.util.regex.Matcher;
+ import java.util.regex.Pattern;
+ 
+ public class UserRegistrationMain {
+	    public static void main(String[] args) {
+	       Validator validator = new Validator();
+	       validator.firstName();
+	    }
+	}
---
+++ src/com/bridgelabz/rejex/Validator.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.rejex;
+
+import java.util.Scanner;
+
+public class Validator {
+	Scanner sc = new Scanner(System.in);
+
+	RejexValidation regexValidation = new RejexValidation();
+
+	public String firstName() {
+		System.out.println();
+		System.out.println("Enter First Name:");
+		String firstName = sc.nextLine();
+		boolean result = regexValidation.validate(firstName);
+		if (result) {
+			System.out.println("valid name");
+			return firstName;
+		} else {
+			System.out.println("Name is not valide \n Enter Correct Name");
+			return firstName();
+		}
+	}
+}
